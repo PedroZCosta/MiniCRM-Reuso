39e8fcd8d35bb471288ef6c22e83ee10b969062f
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2026-09-10 18:43:19 -0300

    chore: projeto inicial gerado pelo Spring Initializr

---
+++ src/main/java/com/miniCRM/miniCRM/MiniCrmApplication.java
@@ -0,0 +1,13 @@
+package com.miniCRM.miniCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCrmApplication.class, args);
+	}
+
+}
